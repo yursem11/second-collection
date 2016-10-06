e07e767828329cd56312a5231bfb679df3fb957a
Yurii <yursem@meta.ua>
2016-10-06 04:31:28 +0000

    add new files

---
+++ index.js
@@ -0,0 +1,17 @@
+function fibonacci (n){
+    var num;
+    if (n >= 2) {
+        num = fibonacci(n-1) + fibonacci(n-2);
+            }
+            else {
+                num = n;
+            }
+}
+alert(fibonacci(10));
+
+/*var n = 20;
+var fibonacci = [0, 1];
+for (i = 2; i < n; i ++) {
+    fibonacci [i] = fibonacci[i-1] + fibonacci[i-2];
+    }
+    console.log(fibonacci.slice(0,n));*/
