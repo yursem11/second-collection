dbcef600949c02ac7462daf11195ff5cd8bc7bbc
Yurii <yursem@meta.ua>
2016-10-06 13:19:09 +0000

    add new files

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*function fibonacci (n){
-    var num;
-    if (n >= 2) {
-        num = fibonacci(n-1) + fibonacci(n-2);
-            }
-            else {
-                num = n;
-            }
-}
-alert(fibonacci(10));*/
-
 var n = 20;
 var fibonacci = [1, 2];
 for (i = 2; i < n; i ++) {
